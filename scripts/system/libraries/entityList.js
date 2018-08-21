414e1e7a7f9fafff616e8fbffbcdfed707e7529d
Ryan Huffman <ryanhuffman@gmail.com>
2018-08-21 09:30:19 -0700

    CP

--- scripts/system/libraries/entityList.js
+++ scripts/system/libraries/entityList.js
@@ -78,6 +78,7 @@ EntityListTool = function(shouldUseEditTabletApp) {
         PROFILE("Script-JSON.stringify", function() {
             dataString = JSON.stringify(data);
         });
+        console.log("Length: ", dataString.length, data.type);
         PROFILE("Script-emitScriptEvent", function() {
             webView.emitScriptEvent(dataString);
             if (entityListWindow.window) {
@@ -145,7 +146,9 @@ EntityListTool = function(shouldUseEditTabletApp) {
             PROFILE("getProperties", function() {
             for (var i = 0; i < ids.length; i++) {
                 var id = ids[i];
-                var properties = Entities.getEntityProperties(id);
+                //var properties = Entities.getEntityProperties(id);
+                var properties = Entities.getEntityProperties(id, ['name', 'type', 'locked',
+                    'visible', 'renderInfo', 'type', 'modelURL', 'materialURL', 'script']);
 
                 if (!filterInView || Vec3.distance(properties.position, cameraPosition) <= searchRadius) {
                     var url = "";
